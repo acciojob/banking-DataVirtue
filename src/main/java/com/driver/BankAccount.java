f1bb063c906c3cd69e5696adbb9c0e598af9c1ce
Akshay Pawar <akshaypawar97@gmail.com>
2023-06-17 11:16:36 +0530

    first commit

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -6,7 +6,41 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
+    public BankAccount() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
+    public BankAccount(String name, double balance, double minBalance) throws Exception {
+
+        if(balance<minBalance)
+            throw new Exception("Insufficient Balance");
+
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
 
     }
 
@@ -14,17 +48,40 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        if(digits*9<sum)
+            throw new Exception("Account Number can not be generated");
 
-        return null;
+        if(sum<0)
+            throw new Exception("Account Number can not be generated");
+
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while(count<digits){
+            if(sum>=9) {
+                sb.append("9");
+                sum-=9;
+            }else {
+                sb.append(sum);
+                sum -= sum;
+            }
+            count++;
+        }
+
+        return sb.toString();
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance+=amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        if(balance - amount<minBalance)
+            throw new Exception("Insufficient Balance");
+
+        balance-=amount;
 
     }
 
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,10 +1,20 @@
 package com.driver;
 
+import java.util.Stack;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
+
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000);
+
+        this.tradeLicenseId = tradeLicenseId;
+        validateLicenseId();
+
+
+
 
     }
 
@@ -13,6 +23,29 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<tradeLicenseId.length();i++){
+
+            while(!sb.isEmpty() && !st.isEmpty() && sb.charAt(sb.length()-1)!=st.peek()){
+                sb.append(st.pop());
+            }
+            if(!sb.isEmpty() && sb.charAt(sb.length()-1)==tradeLicenseId.charAt(i))
+                st.push(tradeLicenseId.charAt(i));
+            else
+                sb.append(tradeLicenseId.charAt(i));
+            System.out.println(sb);
+        }
+
+        while(!sb.isEmpty() && !st.isEmpty() && sb.charAt(sb.length()-1)!=st.peek()){
+            sb.append(st.pop());
+        }
+        if(!st.isEmpty())
+            throw new Exception("Valid License can not be generated");
+
+        tradeLicenseId = sb.toString();
+
+
 
     }
 
